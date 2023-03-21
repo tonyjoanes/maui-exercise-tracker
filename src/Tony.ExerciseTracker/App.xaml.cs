c51599e8bd1bce8a70b12f7fbcc187b20929f10a
Tony Joanes <tonyjoanes@outlook.com>
2023-03-21 20:35:11 +0000

    Initial project

---
+++ src/Tony.ExerciseTracker/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace Tony.ExerciseTracker;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		var navPage = new NavigationPage(new MainPage());
+
+		navPage.BackgroundColor = Colors.Chocolate;
+		navPage.BarTextColor = Colors.Red;
+
+		MainPage = navPage;
+	}
+}
